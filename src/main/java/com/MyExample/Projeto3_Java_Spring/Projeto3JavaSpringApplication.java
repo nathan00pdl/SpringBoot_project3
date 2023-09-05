8ab7e9b26970414902e2de8d027cde34b012fe06
Nathan Paiva <nathanpaiva00@gmail.com>
2023-09-05 13:34:18 -0300

    Project Created

---
+++ src/main/java/com/MyExample/Projeto3_Java_Spring/Projeto3JavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.MyExample.Projeto3_Java_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto3JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto3JavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyExample/Projeto3_Java_Spring/Projeto3JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyExample.Projeto3_Java_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto3JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
