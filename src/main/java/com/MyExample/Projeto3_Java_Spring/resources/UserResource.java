e2b833a56587b8211ef9431604cd48643bb7196f
Nathan Paiva <nathanpaiva00@gmail.com>
2023-09-05 13:55:59 -0300

    Entity User and REST ok

--- src/main/java/com/MyExample/Projeto3_Java_Spring/resources/UserResource.java
+++ src/main/java/com/MyExample/Projeto3_Java_Spring/resources/UserResource.java
@@ -1,5 +1,21 @@
 package com.MyExample.Projeto3_Java_Spring.resources;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.MyExample.Projeto3_Java_Spring.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
 
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		return ResponseEntity.ok().body();
+	}
+	
 }
